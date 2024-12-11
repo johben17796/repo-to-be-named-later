7da895e92489a8e8007aa7effc15fa9f4762e73f
Ben Johnson <johben17796@gmail.com>
2024-12-10 21:19:17 -0600

    test

--- video-game-searcher/src/App.tsx
+++ video-game-searcher/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Four Tet Project</h1>
+      <h1>Four Tet Project TEXT CHANGE ONE ISEHTGSIFGSFDG</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
