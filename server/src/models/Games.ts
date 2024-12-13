0fa05becb2d3b075443bbcb12a036915356ac53a
Ben Johnson <johben17796@gmail.com>
2024-12-12 21:18:00 -0600

    This is on the error page branch because I forgot to close it :)

---
+++ server/src/models/Games.ts
@@ -0,0 +1,48 @@
+import { DataTypes, Sequelize, Model} from 'sequelize';
+
+interface GameAttributes {
+    game_id: number;
+    name: string;
+    slug: string;
+
+}
+
+interface GameCreationAttributes extends GameAttributes {}
+
+
+export class Game extends Model<GameAttributes, GameCreationAttributes> implements GameAttributes {
+    public game_id: number;
+    public name: string;
+    public slug: string;
+
+
+}
+
+export function GameFactory(sequelize: Sequelize): typeof Game {
+  Game.init(
+    {
+      game_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      slug: {
+        type: DataTypes.STRING,
+      },
+
+    },
+    {
+      sequelize,
+
+      tableName: 'Game',
+      timestamps: false,
+      underscored: true,
+      freezeTableName: true,
+    }
+  );
+
+  return Game;
+}
