93b23ec31393024a42e6ab81199a19a5c929a781
Christopher Makousky <christopher.makousky@gmail.com>
2024-12-16 21:58:34 -0600

    indentation formatting in RAWG.ts

--- server/src/routes/api/RAWG.ts
+++ server/src/routes/api/RAWG.ts
@@ -7,19 +7,19 @@ const router = express.Router();
 // GET /games - Get games from RAWG
 router.get('/', async (_req: Request, res: Response) => {
   try {
-      // console.log(process.env.RAWG_API_KEY);
-      const response = await fetch(`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}`);
-      //console.log('Response:', response);
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error('invalid API response, check the network tab');
-      }
-      //console.log('Data:', data);
+    // console.log(process.env.RAWG_API_KEY);
+    const response = await fetch(`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}`);
+    //console.log('Response:', response);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    //console.log('Data:', data);
 
-      //return data;
+    //return data;
 
-      //Express returns the "data" on the "res" object
-      res.send(data);
+    //Express returns the "data" on the "res" object
+    res.send(data);
   } catch (err) {
     console.log('an error occurred', err);
     res.json();
