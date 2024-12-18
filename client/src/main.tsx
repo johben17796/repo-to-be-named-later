3d817f1e7c2f7d362c51872f6dcb9198b47d4998
elele20005 <elele20005@gmail.com>
2024-12-18 14:15:33 -0600

    initial commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-//imports
+/*//imports
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -39,4 +39,16 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+) */
+
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
