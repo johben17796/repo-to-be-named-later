686ae2a74b50897d41e0600fbf45493fdaa11807
Christopher Makousky <christopher.makousky@gmail.com>
2024-12-17 19:45:21 -0600

    now able to search RAWG for a game by name

--- server/src/routes/api/RAWG.ts
+++ server/src/routes/api/RAWG.ts
@@ -5,21 +5,37 @@ import type { Request, Response } from 'express';
 const router = express.Router();
 
 // GET /games - Get games from RAWG
-router.get('/', async (_req: Request, res: Response) => {
+router.get('/games', async (_req: Request, res: Response) => {
   try {
     // console.log(process.env.RAWG_API_KEY);
+    console.log(`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}`);
     const response = await fetch(`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}`);
     //console.log('Response:', response);
     const data = await response.json();
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
-    //console.log('Data:', data);
-
-    //return data;
-
     //Express returns the "data" on the "res" object
-    res.send(data);
+    res.status(200).send(data);
+  } catch (err) {
+    console.log('an error occurred', err);
+    res.json();
+  }
+});
+
+// GET /gamesByName/:name - Get games from RAWG by name
+router.get('/gamesByName/:name', async (req: Request, res: Response) => {
+  try {
+    // console.log(process.env.RAWG_API_KEY);
+    console.log(`https://api.rawg.io/api/games?search=${req.params.name}&search_exact=true&key=${process.env.RAWG_API_KEY}`);
+    const response = await fetch(`https://api.rawg.io/api/games?search=${req.params.name}&search_exact=true&key=${process.env.RAWG_API_KEY}`);
+    //console.log('Response:', response);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    //Express returns the "data" on the "res" object
+    res.status(200).send(data);
   } catch (err) {
     console.log('an error occurred', err);
     res.json();
