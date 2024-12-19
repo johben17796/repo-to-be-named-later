703d3829ab463dfe5f8ad2bb7bf81a68adfaaff2
Christopher Makousky <christopher.makousky@gmail.com>
2024-12-19 15:22:15 -0600

    added RAWG search api and interface to the client

---
+++ client/src/api/searchRAWG.tsx
@@ -0,0 +1,43 @@
+// import { ApiMessage } from "../interfaces/ApiMessage";
+import { RawgData } from "../interfaces/rawgData";
+
+const retrieveGames = async () => {
+    try {
+      const response = await fetch('/api/RAWG/', {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json();
+  
+      if(!response.ok) {
+        throw new Error('invalid RAWG API response, check network tab!');
+      }
+  
+      return data;
+    } catch (err) {
+      console.log('Error from data retrieval:', err);
+      return [];
+    }  
+  };
+  
+  const retrieveGamesByName = async (slug: string | null): Promise<RawgData> => {
+    try {
+      const response = await fetch(`/api/RAWG/gamesByName/${slug}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      const data = await response.json();
+      if(!response.ok) {
+        throw new Error('invalid RAWG API response, check network tab!');
+      }
+  
+      return data;
+    } catch (err) {
+      console.log('Error from data retrieval:', err);
+      return Promise.reject('No matches for that search criteria');
+    }
+  };
+
+export { retrieveGames, retrieveGamesByName };
---
+++ client/src/interfaces/RawgData.tsx
@@ -0,0 +1,5 @@
+export interface RawgData {
+    game_id: number | null;
+    name: string | null;
+    slug: string | null;
+  }
