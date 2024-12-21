3dc3c2f6bc921aa1ede100eafdee1ece613772aa
Christopher Makousky <christopher.makousky@gmail.com>
2024-12-21 10:23:48 -0600

    Added ability to retrieve game info from RAWG based on game id number.

--- client/src/interfaces/RawgData.tsx
+++ client/src/interfaces/RawgData.tsx
@@ -1,5 +1,5 @@
 export interface RawgData {
-    game_id: number | null;
+    id: number | null;
     name: string | null;
     slug: string | null;
     background_image: string | null;
--- server/src/routes/api/RAWG.ts
+++ server/src/routes/api/RAWG.ts
@@ -7,7 +7,6 @@ const router = express.Router();
 // GET /games - Get games from RAWG
 router.get('/games', async (_req: Request, res: Response) => {
   try {
-    // console.log(process.env.RAWG_API_KEY);
     console.log(`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}`);
     const response = await fetch(`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}`);
     //console.log('Response:', response);
@@ -26,7 +25,6 @@ router.get('/games', async (_req: Request, res: Response) => {
 // GET /gamesByName/:name - Get games from RAWG by name
 router.get('/gamesByName/:name', async (req: Request, res: Response) => {
   try {
-    // console.log(process.env.RAWG_API_KEY);
     console.log(`https://api.rawg.io/api/games?search=${req.params.name}&search_exact=true&key=${process.env.RAWG_API_KEY}`);
     const response = await fetch(`https://api.rawg.io/api/games?search=${req.params.name}&search_exact=true&key=${process.env.RAWG_API_KEY}`);
     //console.log('Response:', response);
@@ -41,5 +39,42 @@ router.get('/gamesByName/:name', async (req: Request, res: Response) => {
     res.json();
   }
 });
+
+// GET /gameInfo/:id - Get game info based on game_id number
+router.get('/gameInfo/:id', async (req: Request, res: Response) => {
+  try {
+    console.log(`https://api.rawg.io/api/games/${req.params.id}?key=${process.env.RAWG_API_KEY}`);
+    const response = await fetch(`https://api.rawg.io/api/games/${req.params.id}?key=${process.env.RAWG_API_KEY}`);
+    //console.log('Response:', response);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    //Express returns the "data" on the "res" object
+    res.status(200).send(data);
+  } catch (err) {
+    console.log('an error occurred', err);
+    res.json();
+  }
+});
+
+// GET /gameInfoSlug/:slug - Get game info based on game slug
+router.get('/gameInfoSlug/:slug', async (req: Request, res: Response) => {
+  try {
+    console.log(`https://api.rawg.io/api/games/${req.params.slug}?key=${process.env.RAWG_API_KEY}`);
+    const response = await fetch(`https://api.rawg.io/api/games/${req.params.slug}?key=${process.env.RAWG_API_KEY}`);
+    //console.log('Response:', response);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    //Express returns the "data" on the "res" object
+    res.status(200).send(data);
+  } catch (err) {
+    console.log('an error occurred', err);
+    res.json();
+  }
+});
+
 
 export { router as rawgRouter };
